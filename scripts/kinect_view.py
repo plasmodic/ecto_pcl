c3da819165e8110802f1f01124e2f036e2f6473d
Troy D. Straszheim <straszheim@willowgarage.com>
2011-07-06 15:32:47 -0700

    woot

--- scripts/kinect_view.py
+++ scripts/kinect_view.py
@@ -6,9 +6,12 @@ plasm = ecto.Plasm()
 
 grabber = ecto_pcl.KinectGrabber("Grabber")
 
+voxgrid = ecto_pcl.VoxelGrid("VGrid", leaf_size=0.04)
+
 viewer = ecto_pcl.CloudViewer("Viewer", window_name="Clouds!")
 
-plasm.connect(grabber[:] >> viewer[:])
+plasm.connect(grabber[:] >> voxgrid[:],
+              voxgrid[:] >> viewer[:])
 
 sched = ecto.schedulers.Threadpool(plasm)
 sched.execute()
