d5904c13504c7b70fc24ce4a7f6ba2bb794bf14f
Mike Ferguson <mferguson@willowgarage.com>
2011-07-11 13:36:05 -0700

    add filters, segmentation, tabletop segmentation example

--- src/PassThrough.cpp
+++ src/PassThrough.cpp
@@ -6,10 +6,13 @@ struct PassThrough
   static void declare_params(ecto::tendrils& params)
   {
     // filter params
+    pcl::PassThrough<cloud_t::PointType> default_;
     params.declare<std::string> ("filter_field_name", "The name of the field to use for filtering.", "");
-    params.declare<double> ("filter_limit_min", "Minimum value for the filter.", 0.0);
-    params.declare<double> ("filter_limit_max", "Maximum value for the filter.", 0.0);
-    params.declare<bool> ("filter_limit_negative", "To negate the limits or not.", false);
+    double filter_limit_min, filter_limit_max;
+    default_.getFilterLimits(filter_limit_min, filter_limit_max);
+    params.declare<double> ("filter_limit_min", "Minimum value for the filter.", filter_limit_min);
+    params.declare<double> ("filter_limit_max", "Maximum value for the filter.", filter_limit_max);
+    params.declare<bool> ("filter_limit_negative", "To negate the limits or not.", default_.getFilterLimitsNegative());
   }
 
   static void declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
@@ -24,12 +27,12 @@ struct PassThrough
   {
     //set filter details.
     std::string filter_field_name = params.get<std::string> ("filter_field_name");
-    filter_.setFilterFieldName(filter_field_name);
+    impl_.setFilterFieldName(filter_field_name);
     double filter_limit_min = params.get<double> ("filter_limit_min");
     double filter_limit_max = params.get<double> ("filter_limit_max");
-    filter_.setFilterLimits(filter_limit_min,filter_limit_max);
+    impl_.setFilterLimits(filter_limit_min,filter_limit_max);
     bool filter_limit_negative = params.get<bool> ("filter_limit_negative");
-    filter_.setFilterLimitsNegative(filter_limit_negative);
+    impl_.setFilterLimitsNegative(filter_limit_negative);
 
     //set in/out.
     input_ = inputs.at("input");
@@ -40,13 +43,13 @@ struct PassThrough
   {
     cloud_t::Ptr cloud ( new cloud_t() );
     //filter it.
-    filter_.setInputCloud(*input_);
-    filter_.filter(*cloud);
+    impl_.setInputCloud(*input_);
+    impl_.filter(*cloud);
     //set the output.
-    *output_ = (cloud_t::ConstPtr) cloud;
+    *output_ = cloud;
     return 0;
   }
-  pcl::PassThrough<cloud_t::PointType> filter_;
+  pcl::PassThrough<cloud_t::PointType> impl_;
   ecto::spore<cloud_t::ConstPtr> input_;
   ecto::spore<cloud_t::ConstPtr> output_;
 
--- src/ecto_pcl.hpp
+++ src/ecto_pcl.hpp
@@ -4,7 +4,13 @@
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/PointIndices.h>
+#include <pcl/ModelCoefficients.h>
 
 typedef pcl::PointCloud<pcl::PointXYZRGB> cloud_t;
+typedef pcl::PointCloud<pcl::Normal> normals_t;
+typedef pcl::PointIndices indices_t;
+typedef pcl::ModelCoefficients model_t;
+
 using ecto::tendrils;
 
