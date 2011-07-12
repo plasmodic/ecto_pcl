c822edbc64fcf2cb640c99a1922e1d8bd243e32a
Troy Straszheim <straszheim@willowgarage.com>
2011-07-11 23:30:02 -0700

    Control destruction a little better.  Clean up threading code, no need for all that interrupting and spinning. Shutdown is kind of slow, I think due to some strangeness down in pcl-land.

--- src/KinectGrabber.cpp
+++ src/KinectGrabber.cpp
@@ -25,64 +25,64 @@ class SimpleKinectGrabber
 public:
   SimpleKinectGrabber() 
     : thread_(boost::ref(*this))
-  { }
+  { 
+    runmutex_.lock(); // when this gets unlocked in the destructor, the thread unblocks
+  }
 
   ~SimpleKinectGrabber() { }
 
+  void stop() {
+    runmutex_.unlock();
+    thread_.join();
+  }
+
   void operator ()()
   {
-
-    boost::scoped_ptr<pcl::Grabber> interface(new pcl::OpenNIGrabber());
+    boost::scoped_ptr<pcl::Grabber> interface(new pcl::OpenNIGrabber);
 
     boost::function<void(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> point_cloud_cb =
         boost::bind(&SimpleKinectGrabber::cloud_cb_, this, _1);
 
-    boost::function<void(const boost::shared_ptr<openni_wrapper::Image>&,
-                         const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant)> image_depth_cb =
-        boost::bind(&SimpleKinectGrabber::image_depth_cb_, this, _1, _2, _3);
-
-    //interface->
     boost::signals2::connection c = interface->registerCallback(point_cloud_cb);
 
     interface->start();
 
-    while (!thread_.interruption_requested())
-      {
-        boost::thread::yield();
-      }
+    runmutex_.lock(); // blocks until the destructor unlocks it
+    runmutex_.unlock(); // unlock it again... why does this stop a crash?
 
     c.disconnect();
-    std::cerr << "Stopping" << std::endl;
 
     interface->stop();
   }
+
 
   /**
    * \brief don't hang on to this cloud!! or it won't get updated.
    */
   cloud_t::ConstPtr getLatestCloud()
   {
-    boost::mutex::scoped_lock lock(mutex_);
+    boost::mutex::scoped_lock lock(datamutex_);
+
+    while (!cloud_)
+      cond_.wait(lock);
+
     cloud_t::ConstPtr p = cloud_;
     cloud_.reset();
     return p;
   }
-  //depth callback.
-  void image_depth_cb_(const boost::shared_ptr<openni_wrapper::Image>&,
-                       const boost::shared_ptr<openni_wrapper::DepthImage>&, float constant)
-  {
 
-  }
   void cloud_cb_(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
   {
-    boost::mutex::scoped_lock lock(mutex_);
+    boost::lock_guard<boost::mutex> lock(datamutex_);
     cloud_ = cloud;
+    cond_.notify_one();
   }
 
-  boost::mutex mutex_;
+  boost::mutex datamutex_, runmutex_;
+  boost::condition_variable cond_;
+
   cloud_t::ConstPtr cloud_;
   boost::thread thread_;
-
 };
 ECTO_CELL(ecto_pcl, SimpleKinectGrabber, "SimpleKinectGrabber", "Simple kinect grabber");
 
@@ -94,19 +94,24 @@ struct KinectGrabber
     outputs.declare<cloud_t::ConstPtr> ("output", "An rgb xyz point cloud from the kinect");
   }
 
+  int configure(const tendrils& parameters, tendrils& inputs, tendrils& outputs)
+  {
+    impl_.reset(new SimpleKinectGrabber);
+  }
+
   int process(const tendrils& /*inputs*/, tendrils& outputs)
   {
     cloud_t::ConstPtr p;
-    while (!p)
-      {
-        p = grabber_.getLatestCloud();
-        boost::thread::yield();
-      }
+    p = impl_->getLatestCloud();
     outputs.get<cloud_t::ConstPtr> ("output") = p;
     return 0;
   }
 
-  SimpleKinectGrabber grabber_;
+  ~KinectGrabber() { impl_->stop(); }
+
+
+
+  boost::scoped_ptr<SimpleKinectGrabber> impl_;
 };
 
 ECTO_CELL(ecto_pcl, KinectGrabber, "KinectGrabber", "Grabber from kinect");
