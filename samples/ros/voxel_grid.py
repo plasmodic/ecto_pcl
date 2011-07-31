5428f8b9ef875d5c7c406a6ca848eee18ed22bc1
Mike Ferguson <mferguson@willowgarage.com>
2011-07-30 18:25:00 -0700

    tabletop segmentation and voxel grid to samples

---
+++ samples/ros/voxel_grid.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+A VoxelGrid can be used to downsample very dense clouds.
+This can be especially useful when estimating normals. 
+"""
+
+import ecto, ecto_pcl, ecto_pcl_ros
+
+PointCloudSub = ecto_sensor_msgs.Subscriber_PointCloud2
+PointCloudPub = ecto_sensor_msgs.Publisher_PointCloud2
+
+def do_ecto():
+    plasm = ecto.Plasm()
+
+    sub = PointCloudSub("Subscriber", topic_name='/camera/rgb/points')
+    
+    grabber = ecto_pcl_ros.Message2PointCloud(format=ecto_pcl.XYZRGB)
+
+    voxgrid = ecto_pcl.VoxelGrid("VGrid", leaf_size=0.05)
+
+    pcl2msg = ecto_pcl_ros.PointCloud2Message()
+
+    pub = PointCloudPub("Cloud Publisher", topic_name='/ecto_pcl/voxel_grid')    
+
+    plasm.connect(sub['output'] >> grabber[:],
+                  grabber[:] >> voxgrid[:],
+                  voxgrid[:] >> pcl2msg[:],
+                  pcl2msg[:] >> pub[:])
+
+    sched = ecto.schedulers.Threadpool(plasm)
+    sched.execute()
+
+if __name__ == "__main__":
+    ecto_ros.init(sys.argv, "voxel_grid")
+    do_ecto()
+
