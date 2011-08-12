8ec1d7e5cf182e429e5ecba2f3c7ec31c60680a2
Mike Ferguson <mferguson@willowgarage.com>
2011-08-12 02:40:44 -0700

    Add new cloud merger and demo

---
+++ samples/ros/merge_clouds.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+MergeClouds allows you to merge 2 clouds together!
+"""
+
+import ecto, ecto_pcl, ecto_pcl_ros
+import ecto_ros, ecto_sensor_msgs
+import sys
+
+PointCloudSub = ecto_sensor_msgs.Subscriber_PointCloud2
+PointCloudPub = ecto_sensor_msgs.Publisher_PointCloud2
+
+def do_ecto():
+    plasm = ecto.Plasm()
+
+    sub = PointCloudSub("Subscriber", topic_name='/camera/rgb/points')
+    
+    grabber = ecto_pcl_ros.Message2PointCloud("Message2Cloud", format=ecto_pcl.XYZRGB)
+
+    p1 = ecto_pcl.PassThrough("Pass", filter_field_name="z", filter_limit_max=2.0)
+    p2 = ecto_pcl.PassThrough("Pass2", filter_field_name="z", filter_limit_min=3.0, filter_limit_max=8.0)
+
+    voxgrid = ecto_pcl.VoxelGrid("VGrid", leaf_size=0.1)
+    merge = ecto_pcl.MergeClouds("Merge")
+
+    pcl2msg = ecto_pcl_ros.PointCloud2Message("Cloud2Message")
+
+    pub = PointCloudPub("Cloud Publisher", topic_name='/ecto_pcl/sample_output')    
+
+    plasm.connect(sub['output'] >> grabber[:],
+                  grabber[:] >> p1[:],
+                  grabber[:] >> p2[:],
+                  p1[:] >> voxgrid[:],
+                  voxgrid[:] >> merge["input1"],
+                  p2[:] >> merge["input2"],
+                  merge[:] >> pcl2msg[:],
+                  pcl2msg[:] >> pub[:])
+
+    sched = ecto.schedulers.Threadpool(plasm)
+    sched.execute()
+
+if __name__ == "__main__":
+    ecto_ros.init(sys.argv, "merge_clouds")
+    do_ecto()
+
---
+++ src/io/MergeClouds.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2011, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ecto_pcl.hpp"
+#include "DualFilter.hpp"
+
+struct MergeClouds
+{
+  static void declare_params(ecto::tendrils& params) { }
+  static void declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
+  {
+    outputs.declare<PointCloud> ("output", "Merged cloud.");
+  }
+
+  void configure(const tendrils& params, const tendrils& inputs, const tendrils& outputs)
+  {
+    output_ = outputs["output"];
+  }
+  
+  template <typename Point>
+  int process(const tendrils& inputs, const tendrils& outputs, 
+              boost::shared_ptr<const pcl::PointCloud<Point> >& input1, boost::shared_ptr<const pcl::PointCloud<Point> >& input2)
+  {
+    pcl::PointCloud<Point> cloud;
+    cloud = *input1;
+    cloud += *input2;
+    *output_ = xyz_cloud_variant_t(cloud.makeShared());
+    return 0;
+  }
+  
+  ecto::spore<PointCloud> output_;
+};
+
+ECTO_CELL(ecto_pcl, ecto::pcl::DualFilterCell<MergeClouds>, "MergeClouds", "Merges two clouds of the same type.");
+
