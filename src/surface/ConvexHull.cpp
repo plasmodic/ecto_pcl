51747d2dfa8dbe7915cecb059ba39001f1456212
Mike Ferguson <mferguson@willowgarage.com>
2011-08-12 10:52:50 -0700

    port ConvexHul

--- src/surface/ConvexHull.cpp
+++ src/surface/ConvexHull.cpp
@@ -28,89 +28,39 @@
  */
 
 #include "ecto_pcl.hpp"
+#include "pcl_cell.hpp"
 #include <pcl/surface/convex_hull.h>
-
-
-#define DECLARECONVEXHULL(r, data, i, ELEM)                            \
-  BOOST_PP_COMMA_IF(i) pcl::ConvexHull< BOOST_PP_TUPLE_ELEM(2, 0, ELEM) >
-
-typedef boost::variant< BOOST_PP_SEQ_FOR_EACH_I(DECLARECONVEXHULL, ~, ECTO_XYZ_POINT_TYPES) > surface_variant_t;
 
 struct ConvexHull
 {
-  /* used to create a surface */
-  template <template <class> class SurfacePolicy>
-  struct make_surface_variant : boost::static_visitor<surface_variant_t>
-  {
-    template <typename CloudType >
-    surface_variant_t operator()(const CloudType& p) const
-    {
-      return surface_variant_t(SurfacePolicy<typename CloudType::element_type::PointType>());
-    }
-  };
-
-  /* dispatch to handle process */
-  struct surface_dispatch : boost::static_visitor<xyz_cloud_variant_t>
-  {
-    template <typename Surface, typename CloudType>
-    xyz_cloud_variant_t operator()(Surface& f, CloudType& i) const
-    {
-      return impl(f, i, pcl_takes_point_trait<Surface, CloudType>());
-    }
-
-    template <typename Surface, typename CloudType>
-    xyz_cloud_variant_t impl(Surface& f, boost::shared_ptr<const CloudType>& i, boost::true_type) const
-    {
-      CloudType o;
-      f.setInputCloud(i);
-      f.reconstruct(o);
-      return xyz_cloud_variant_t(o.makeShared());
-    }
-
-    template <typename Surface, typename CloudType>
-    xyz_cloud_variant_t impl(Surface& f, CloudType& i, boost::false_type) const
-    {
-      throw std::runtime_error("types aren't the same, you are doing something baaaaaad");
-    }
-  };
-
-  static void declare_params(tendrils& params)
-  {
-    // no params
-  }
+  static void declare_params(ecto::tendrils& params) { }
 
   static void declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
   {
-    inputs.declare<PointCloud> ("input", "The cloud to filter");
-    outputs.declare<PointCloud> ("output", "Feature cloud.");
+    outputs.declare<PointCloud> ("output", "Points that form the the convex hull.");
   }
-
-  ConvexHull() : configured_(false) {}
 
   void configure(const tendrils& params, const tendrils& inputs, const tendrils& outputs)
   {
-    // set in/out.
-    input_ = inputs["input"];
     output_ = outputs["output"];
   }
-
-  int process(const tendrils& inputs, const tendrils& outputs)
-  {
-    xyz_cloud_variant_t cvar = input_->make_variant();
-    if(!configured_){
-      impl_ = boost::apply_visitor(make_surface_variant<pcl::ConvexHull>(), cvar);
-      configured_ = true;
-    }
-    *output_ = boost::apply_visitor(surface_dispatch(), impl_, cvar);
-    return 0;
-  }
   
-  bool configured_;
-  surface_variant_t impl_;
-  ecto::spore<PointCloud> input_;
-  ecto::spore<PointCloud> output_;
+  template <typename Point>
+  int process(const tendrils& inputs, const tendrils& outputs, 
+              boost::shared_ptr<const pcl::PointCloud<Point> >& input)
+  {
+    pcl::ConvexHull<Point> filter;
+    pcl::PointCloud<Point> cloud;
 
+    filter.setInputCloud(input);
+    filter.reconstruct(cloud);
+
+    *output_ = xyz_cloud_variant_t(cloud.makeShared());
+    return ecto::OK;
+  }
+
+  ecto::spore<PointCloud> output_;
 };
 
-ECTO_CELL(ecto_pcl, ConvexHull, "ConvexHull", "Using libqhull library.");
+ECTO_CELL(ecto_pcl, ecto::pcl::PclCell<ConvexHull>, "ConvexHull", "Using libqhull library.");
 
