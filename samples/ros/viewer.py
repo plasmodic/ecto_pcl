bb2f28282189880277f8c8e5075597bca53870c2
Mike Ferguson <mferguson@willowgarage.com>
2011-08-19 18:10:29 -0700

    first pass at NI converter

--- samples/ros/viewer.py
+++ samples/ros/viewer.py
@@ -4,22 +4,19 @@
 This sample shows how to use the cloud viewer with a ROS-based plasm.
 """
 
-import ecto, ecto_pcl, ecto_pcl_ros
-import ecto_ros, ecto_sensor_msgs
-import sys
-
-PointCloudSub = ecto_sensor_msgs.Subscriber_PointCloud2
+import ecto, ecto_pcl
+from ecto_openni import Capture
 
 plasm = ecto.Plasm()
-sub = PointCloudSub("Subscriber", topic_name='/camera/depth_registered/points')
-grabber = ecto_pcl_ros.Message2PointCloud("Message2Cloud", format=ecto_pcl.XYZRGB)
+
+capture = Capture('device')
+verter = ecto_pcl.NiConverter('verter')
 viewer = ecto_pcl.CloudViewer("viewer", window_name="Clouds!")
 
-plasm.connect(sub['output'] >> grabber[:],
-              grabber[:] >> viewer[:])
+plasm.connect(capture[:] >> verter[:],
+              verter[:] >> viewer[:])
 
 if __name__ == "__main__":
-    ecto_ros.init(sys.argv, "passthrough")
     sched = ecto.schedulers.Threadpool(plasm)
     sched.execute()
 
