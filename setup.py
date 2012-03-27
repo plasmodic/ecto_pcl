72a1302af6769d331633fc8f748d8e5d8d15726c
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 23:38:05 -0700

    simplify the CMake

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Ecto PCL',
+      version='1.0.0',
+      description='Ecto bindings for PCL',
+      packages=['ecto_pcl'],
+      package_dir={'':'python'}
+)
