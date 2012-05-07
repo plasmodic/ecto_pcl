80f5125ee2df0b2083470d39767ab5ef28131140
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-07 05:14:19 -0700

    fix the new API

--- src/surface/ConvexHull.cpp
+++ src/surface/ConvexHull.cpp
@@ -56,7 +56,9 @@ namespace ecto {
         ::pcl::PointCloud<Point> cloud;
 
         filter.setInputCloud(input);
+#ifdef PCL_VERSION_GE_140
         filter.setDimension(3);
+#endif
         filter.reconstruct(cloud);
 
         *output_ = xyz_cloud_variant_t(cloud.makeShared());
