9f97861708b628114f98aaf8be50fc8721e763cc
Vincent Rabaud <vrabaud@willowgarage.com>
2012-11-23 17:10:58 -0800

    it's not ecto_opencv ...

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ html_theme_path = ['../../ecto/doc/source/']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'ecto_opencv'
+html_title = 'ecto_pcl'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
