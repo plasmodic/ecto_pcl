40c0fd079270aa9b291f2ab4fb5c8518509f8c42
Vincent Rabaud <vincent.rabaud@gmail.com>
2015-05-17 23:55:10 +0200

    clean tests

--- test/test_import.py
+++ test/test_import.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import ecto_pcl
+import unittest
+
+class TestPcl(unittest.TestCase):
+
+    def test_import(self):
+        print(ecto_pcl)
+
+if __name__ == '__main__':
+    import rosunit
+    rosunit.rosrun('ecto_pcl', 'test_import', TestPcl)
